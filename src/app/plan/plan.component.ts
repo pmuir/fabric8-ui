236eee6f4a02cbd0d562328b4dbd3d808b8ad998
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-07 10:56:22 -0500

    fix(sass): add planner custom styles

--- src/app/plan/plan.component.ts
+++ src/app/plan/plan.component.ts
@@ -1,11 +1,12 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, ViewEncapsulation } from '@angular/core';
 import { Router }            from '@angular/router';
 
+import * as plannerStyles from 'fabric8-planner/src/app/app.component.css';
 
 @Component({
   selector: 'alm-plan',
   templateUrl: 'plan.component.html',
-  styleUrls: ['./plan.component.scss']
+  styles: [ plannerStyles.toString() ]
 })
 export class PlanComponent implements OnInit {
 
