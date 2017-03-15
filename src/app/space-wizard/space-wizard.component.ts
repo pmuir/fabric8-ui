836db565f3214ba47b9936792dbb8e61fb9bec8a
Pete Muir <pmuir@bleepbleep.org.uk>
2017-03-14 23:04:05 -0400

    fix(spaces): correctly create spaces
    * Note that validation is not working for space creation

--- src/app/space-wizard/space-wizard.component.ts
+++ src/app/space-wizard/space-wizard.component.ts
@@ -1,15 +1,29 @@
-import { WizardSteps } from './../shared-component/wizard/wizard-steps';
-import { Wizard } from './../shared-component/wizard/wizard';
-import { Component, OnInit, Input } from '@angular/core';
+import { NgModel } from '@angular/forms';
+import { Component, OnInit, Input, ViewChild } from '@angular/core';
 import { Router } from '@angular/router';
 import { Http } from '@angular/http';
 
-import { SpaceService, Space, ProcessTemplate, SpaceAttributes, Context, Contexts, Notification, NotificationType, Notifications } from 'ngx-fabric8-wit';
-import { Broadcaster, User, HttpService } from 'ngx-login-client';
+import { Observable } from 'rxjs';
+
+import {
+  SpaceService,
+  Space,
+  ProcessTemplate,
+  SpaceAttributes,
+  Context,
+  Contexts,
+  Notification,
+  NotificationType,
+  NotificationAction,
+  Notifications
+} from 'ngx-fabric8-wit';
+import { Broadcaster, User, HttpService, UserService } from 'ngx-login-client';
 
 import { DummyService } from '../shared/dummy.service';
 import { SpaceConfigurator } from './wizard';
 import { Modal } from '../shared-component/modal/modal';
+import { WizardSteps } from './../shared-component/wizard/wizard-steps';
+import { Wizard } from './../shared-component/wizard/wizard';
 
 @Component({
   host: {
@@ -32,6 +46,9 @@ export class SpaceWizardComponent implements OnInit {
   wizardSteps: WizardSteps;
   @Input() host: Modal;
 
+  @ViewChild('name') nameModel: NgModel;
+
+
   private _context: Context;
 
   constructor(
@@ -40,6 +57,7 @@ export class SpaceWizardComponent implements OnInit {
     private broadcaster: Broadcaster,
     private spaceService: SpaceService,
     private notifications: Notifications,
+    private userService: UserService,
     context: Contexts
   ) {
     context.current.subscribe(val => this._context = val);
@@ -56,9 +74,79 @@ export class SpaceWizardComponent implements OnInit {
     } as WizardSteps;
     this.host.closeOnEscape = true;
     this.host.closeOnOutsideClick = false;
+
   }
 
   next() {
+  }
+
+  createSpace() {
+    let space = this.configurator.space;
+    console.log('Creating space', space);
+    space.attributes.name = space.name;
+    this.userService.getUser().switchMap(user => {
+      space.relationships['owned-by'].data.id = user.id;
+      return this.spaceService.create(space);
+    })
+      .subscribe(createdSpace => {
+        this.configurator.space = createdSpace;
+        let actionObservable = this.notifications.message({
+          message: `Your new space is created!`,
+          type: NotificationType.SUCCESS,
+          primaryAction: {
+            name: `Open Space`,
+            title: `Open ${createdSpace.attributes.name}`,
+            id: 'openSpace'
+          } as NotificationAction
+        } as Notification);
+        actionObservable
+          .filter(action => action.id === 'openSpace')
+          .subscribe(action => {
+            this.router
+              .navigate([createdSpace.relationalData.creator.attributes.username, createdSpace.attributes.name]);
+            if (this.host) {
+              this.host.close();
+              this.reset();
+            }
+          });
+        this.wizard.step(this.wizardSteps.forge.index);
+      },
+      err => {
+        if (err.status === 400) {
+          let body = JSON.parse(err.body);
+          if (body.errors) {
+            let errors = body.errors as { code: string; detail: string; status: number; title: string }[];
+            for (let error of errors) {
+              if (error.code === 'bad_parameter') {
+                // Bad value for parameter 'Name': '1735' (expected: 'unique')
+                /*
+                 * Full match	0-59	`Bad value for parameter 'Name': '1735' (expected: 'unique')`
+                 * Group 1.	n/a	`Name`
+                 * Group 2.	n/a	`1735`
+                 * Group 3.	n/a	`unique`
+                 */
+                let details = error.detail.match(/^Bad value for parameter '([^']*)': '([^']*)' \(expected: '([^']*)'\)$/);
+                if (details.length === 4 && details[3] === 'unique' && details[1] === 'Name') {
+                  if (this.nameModel.dirty) {
+                    this.nameModel.valid = false;
+                    this.nameModel.errors['unique'] = true;
+                  }
+                }
+              }
+            }
+          }
+        } else {
+          this.notifications.message({
+            message: `Failed to create "${space.name}"`,
+            type: NotificationType.DANGER
+
+          } as Notification);
+          if (this.host) {
+            this.host.close();
+            this.reset();
+          }
+        }
+      });
   }
 
   reset() {
@@ -72,46 +160,38 @@ export class SpaceWizardComponent implements OnInit {
     space.type = 'spaces';
     space.privateSpace = false;
     space.process = this.dummy.processTemplates[0];
+    space.relationships = {
+      areas: {
+        links: {
+          related: ''
+        }
+      },
+      iterations: {
+        links: {
+          related: ''
+        }
+      },
+      ['owned-by']: {
+        data: {
+          id: '',
+          type: 'identities'
+        }
+      }
+    };
     configurator.space = space;
     this.configurator = configurator;
     this.wizard = new Wizard();
   }
 
   finish() {
-    console.log('finish!', this._context);
-    let space = this.configurator.space;
-    space.attributes.name = space.name;
-    console.log(this._context);
-    if (this._context && this._context.user) {
-      // TODO Implement space name validation
-      // Support organisations as well
-      space.path =
-        this._context.user.attributes.username + '/' + this.convertNameToPath(space.name);
-    } else {
-      throw new Error('Cannot find user');
+    this.router.navigate([
+      this.configurator.space.relationalData.creator.attributes.username,
+      this.configurator.space.attributes.name
+    ]);
+    if (this.host) {
+      this.host.close();
+      this.reset();
     }
-
-    this.spaceService.create(space)
-      .subscribe(
-      (createdSpace) => {
-        if (space.path) {
-          this.router.navigateByUrl(space.path.toString());
-        }
-        if (this.host) {
-          this.host.close();
-          this.reset();
-        }
-      },
-      (err) => {
-        this.notifications.message({
-          message: `Failed to create the collaboration space "${space.name}" because ${err.message}`,
-          type: NotificationType.DANGER
-        } as Notification);
-        if (this.host) {
-          this.host.close();
-          this.reset();
-        }
-      });
   }
 
   cancel() {
